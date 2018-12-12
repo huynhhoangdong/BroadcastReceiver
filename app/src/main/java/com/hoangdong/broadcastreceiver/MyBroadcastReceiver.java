1b6879a206598b654dadc4120c16e69fa5a43414
huynhhoangdong <33679332+huynhhoangdong@users.noreply.github.com>
2018-12-12 16:28:06 +0700

    Initial commit

---
+++ app/src/main/java/com/hoangdong/broadcastreceiver/MyBroadcastReceiver.java
@@ -0,0 +1,17 @@
+package com.hoangdong.broadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "receive phone call", Toast.LENGTH_LONG).show();
+        Log.d("Hello", "onReceive");
+
+    }
+}
